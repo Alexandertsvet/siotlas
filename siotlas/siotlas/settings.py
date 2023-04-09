9342acef89e0ba3418e03766b03390462cd1ea95
Alexandertsvet <tsvetkov1986@mail.ru>
2023-04-09 21:02:28 +0300

    pasword_reset_done

--- siotlas/siotlas/settings.py
+++ siotlas/siotlas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.auth.tokens',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -130,3 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 LOGOUT_REDIRECT_URL = 'posts:index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR/'sent_emails'
+DEFAULT_FROM_EMAIL = 'tsvetkov1986@mail.ru'
--- siotlas/users/urls.py
+++ siotlas/users/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth.views import (
     PasswordResetConfirmView,
     PasswordResetCompleteView,
 )
-
+from users.views import MyPasswordResetView #, MyPasswordResetDoneView
 from django.urls import path
 
 from users import views
@@ -21,8 +21,11 @@ urlpatterns = [
     path('accounts/signup/', views.SignUp.as_view(), name='signup'),
     path('accounts/password_change/', PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change'),
     path('accounts/password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
-    path('accounts/password_reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'),name='password_reset'),
-    path('account/password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/password_reset/', MyPasswordResetView.as_view(
+            template_name='users/password_reset_form.html',
+            email_template_name = 'users/password_reset_email.html',
+            ),name='password_reset'),
+    path('account/password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('account/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('account/reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
